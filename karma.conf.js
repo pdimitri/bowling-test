6a020a0d9aab47e16a71350e9cd7a62d4899b06a
Johann Böhler <jboehler@inovex.de>
2019-02-12 22:12:19 +0100

    Added missing dependencies & updated versions Removed npm-debug.log Added .gitignore entries

--- karma.conf.js
+++ karma.conf.js
@@ -24,12 +24,6 @@ module.exports = function(config) {
     ],
 
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
-
-
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
@@ -60,10 +54,6 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
-  })
-}
+    singleRun: false
+  });
+};
